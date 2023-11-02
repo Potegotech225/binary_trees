9493241b3e9883b0c2e0477486b3cab8a558c9db
Potegotech225 <mothatapotego@gmail.com>
2023-11-02 15:55:48 -0700

    update

---
+++ 101-binary_tree_levelorder_recursive.c
@@ -0,0 +1,69 @@
+#include "binary_trees.h"
+
+/**
+ * struct node_s - a singly linked list
+ * @node: a const binary tree node
+ * @next: points to the next node
+ */
+typedef struct node_s
+{
+	const binary_tree_t *node;
+	struct node_s *next;
+} ll;
+
+ll *append(ll *head, const binary_tree_t *btnode);
+void free_list(ll *head);
+ll *get_children(ll *head, const binary_tree_t *parent);
+void levels_rec(ll *head, void (*func)(int));
+
+/**
+ * binary_tree_levelorder - it goes through a binary tree
+ *                          using level-order traversal.
+ * @tree: A pointer to the root node of the tree to traverse.
+ * @func: A pointer to a function to call for each node.
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	ll *children = NULL;
+
+	func(tree->n);
+	children = get_children(children, tree);
+	levels_rec(children, func);
+
+	free_list(children);
+}
+
+/**
+ * levels_rec - Calls func on all nodes at each level.
+ * @head: A pointer to head of linked list with nodes at one level.
+ * @func: A pointer to a function to call for each node.
+ */
+void levels_rec(ll *head, void (*func)(int))
+{
+	ll *children = NULL, *curr = NULL;
+
+	if (!head)
+		return;
+	for (curr = head; curr != NULL; curr = curr->next)
+	{
+		func(curr->node->n);
+		children = get_children(children, curr->node);
+	}
+	levels_rec(children, func);
+	free_list(children);
+}
+
+/**
+ * get_children - appends children of parent to linked list.
+ * @head: a pointer to head of linked list where children will be appended.
+ * @parent: A pointer to node whose children we want to append.
+ * Return: A pointer to head of linked list of children.
+ */
+ll *get_children(ll *head, const binary_tree_t *parent)
+{
+	if (parent->left)
+		head = append(head, parent->left);
+	if (parent->right)
+		head = append(head, parent->right);
+	return (head);
+}
